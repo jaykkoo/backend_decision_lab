ba73f4df41209e7b5c5f24e96b2ece31136665c6
Adrien <adrien.gutleben@gmail.com>
2026-02-05 15:38:38 +0100

    try

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -2,7 +2,18 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
+
+if os.environ.get("KUBERNETES_SERVICE_HOST"):
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = [
+        h.strip()
+        for h in os.getenv(
+            "DJANGO_ALLOWED_HOSTS",
+            "localhost,127.0.0.1"
+        ).split(",")
+        if h.strip()
+    ]
 
 # Sécurité minimale prod
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
